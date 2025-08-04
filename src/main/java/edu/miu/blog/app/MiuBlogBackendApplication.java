39ffd1f82650ba79a98a4a2ba7f46da12f272b90
Leonardo Medina <leoz.31@hotmail.com>
2025-08-04 14:41:12 -0500

    feat(back)init

--- src/main/java/edu/miu/blog/app/MiuBlogBackendApplication.java
+++ src/main/java/edu/miu/blog/app/MiuBlogBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiuBlogBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Classpath: _______________ " + System.getProperty("java.class.path"));
 		SpringApplication.run(MiuBlogBackendApplication.class, args);
 	}
 
